4dcdd3bef8cb2a3a8ecd2e3c094770fc0084ae57
JAGRUTI2024 <jagruti.surange@gmail.com>
2025-02-24 12:45:37 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name="Information_Retrieval",
+    version= "0.0.0.1",
+    author=" Jagruti Surange",
+    author_email="jagruti.surange@gmail.com",
+    description="Information Retrieval System",
+    packages= find_packages(),
+    install_requires=[],
+    )
